95988901e6dfe7919265104aac3ee24e06b44263
Yaroslav_Shatalov <yaroslav.shatalov@activeeon.com>
2020-05-21 10:25:00 +0200

    remove template objects

--- src/main/java/org/myproject/parking/rest/ParkingRest.java
+++ src/main/java/org/myproject/parking/rest/ParkingRest.java
@@ -5,18 +5,12 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.myproject.parking.model.Invoice;
 import org.myproject.parking.model.ParkingSpot;
-import org.myproject.parking.model.User;
 import org.myproject.parking.model.vehicle.Vehicle;
 import org.myproject.parking.service.ParkingService;
-import org.myproject.parking.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Collection;
-import java.util.Optional;
 
 
 /**
