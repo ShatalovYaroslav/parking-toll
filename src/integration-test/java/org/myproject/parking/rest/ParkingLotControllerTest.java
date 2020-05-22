5fbcf76b0ea475935f05fbd15d2a9df119a1f115
Yaroslav_Shatalov <yaroslav.shatalov@activeeon.com>
2020-05-22 22:36:56 +0200

    add parking lot rest controller integration tests

---
+++ src/integration-test/java/org/myproject/parking/rest/ParkingLotControllerTest.java
@@ -0,0 +1,130 @@
+
+package org.myproject.parking.rest;
+
+import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.http.ContentType;
+import com.jayway.restassured.response.Response;
+import org.apache.http.HttpStatus;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.myproject.parking.Application;
+import org.myproject.parking.dto.ParkingLotMetadata;
+import org.myproject.parking.pricing.PolicyType;
+import org.myproject.parking.util.ParkingLotStartupFixture;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static com.jayway.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.annotation.DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD;
+
+
+@ActiveProfiles("test")
+@DirtiesContext(classMode = AFTER_EACH_TEST_METHOD)
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = {Application.class})
+@WebIntegrationTest(randomPort = true)
+public class ParkingLotControllerTest extends AbstractRestTest {
+
+    private String REST_SERVICE_URI;
+
+    @Before
+    public void configureRestAssured() {
+        REST_SERVICE_URI = "http://localhost:" + serverPort + "/parking/lot/";
+        RestAssured.port = serverPort;
+    }
+    @Autowired
+    ParkingLotStartupFixture parkingLotStartupFixture;
+
+    @Test
+    public void createParkingLot() {
+        ParkingLotMetadata parkingLotMetadata = parkingLotStartupFixture.createParking();
+
+        Response response = given().body(parkingLotMetadata)
+                .header("Accept", ContentType.JSON.getAcceptHeader())
+                .header("Content-Type", ContentType.JSON)
+                .when()
+                .post(REST_SERVICE_URI);
+
+        response.then()
+                .assertThat()
+                .statusCode(HttpStatus.SC_CREATED)
+                .body("name", is(parkingLotMetadata.getName()))
+                .body("pricingConfig.policyType", is(PolicyType.FIXED_PLUS.toString()))
+                .body("spots", hasSize(7))
+                .body("spots[0].free", is(true));
+    }
+
+    @Test
+    public void getParkingLot() {
+        ParkingLotMetadata parkingLotMetadata = parkingLotStartupFixture.createParking();
+
+        //add parking lot
+         given().body(parkingLotMetadata)
+                .header("Accept", ContentType.JSON.getAcceptHeader())
+                .header("Content-Type", ContentType.JSON)
+                .when()
+                .post(REST_SERVICE_URI);
+
+        //get parking lot
+        given()
+                .header("Accept", ContentType.JSON.getAcceptHeader())
+                .header("Content-Type", ContentType.JSON)
+                .pathParam("parkingLotId", parkingLotMetadata.getParkingId())
+                .when()
+                .get(REST_SERVICE_URI + "{parkingLotId}/")
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.SC_OK)
+                .body("name", is(parkingLotMetadata.getName()))
+                .body("pricingConfig.policyType", is(PolicyType.FIXED_PLUS.toString()))
+                .body("spots", hasSize(7))
+                .body("spots[0].free", is(true));
+    }
+
+    @Test
+    public void removeParkingLot() {
+        ParkingLotMetadata parkingLotMetadata = parkingLotStartupFixture.createParking();
+
+        //add parking lot
+        given().body(parkingLotMetadata)
+                .header("Accept", ContentType.JSON.getAcceptHeader())
+                .header("Content-Type", ContentType.JSON)
+                .when()
+                .post(REST_SERVICE_URI);
+
+        //get parking lot
+        given()
+                .header("Accept", ContentType.JSON.getAcceptHeader())
+                .header("Content-Type", ContentType.JSON)
+                .pathParam("parkingLotId", parkingLotMetadata.getParkingId())
+                .when()
+                .delete(REST_SERVICE_URI+ "{parkingLotId}/")
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.SC_OK)
+                .body("name", is(parkingLotMetadata.getName()))
+                .body("pricingConfig.policyType", is(PolicyType.FIXED_PLUS.toString()))
+                .body("spots", hasSize(7))
+                .body("spots[0].free", is(true));
+
+        //get parking lot
+        given()
+                .header("Accept", ContentType.JSON.getAcceptHeader())
+                .header("Content-Type", ContentType.JSON)
+                .pathParam("parkingLotId", parkingLotMetadata.getParkingId())
+                .when()
+                .get(REST_SERVICE_URI+ "{parkingLotId}/")
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.SC_NOT_FOUND);
+    }
+
+}
+
