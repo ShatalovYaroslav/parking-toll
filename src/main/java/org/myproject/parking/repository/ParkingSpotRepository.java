4cfce71822b495a9933c3cb8818fb139866afc23
Yaroslav_Shatalov <yaroslav.shatalov@activeeon.com>
2020-05-23 22:17:22 +0200

    use Parking Spot Repository for Spot Service

--- src/main/java/org/myproject/parking/repository/ParkingSpotRepository.java
+++ src/main/java/org/myproject/parking/repository/ParkingSpotRepository.java
@@ -1,14 +1,28 @@
 
 package org.myproject.parking.repository;
 
-import org.myproject.parking.model.ParkingLot;
 import org.myproject.parking.model.ParkingSpot;
+import org.myproject.parking.model.vehicle.VehicleType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 
 @Repository
-public interface ParkingSpotRepository extends JpaRepository<ParkingSpot, Integer> {
+public interface ParkingSpotRepository extends JpaRepository<ParkingSpot, Integer>,
+        JpaSpecificationExecutor<ParkingSpot> {
 
-    ParkingLot findOneBySpotId(Integer spotId);
+    ParkingSpot findOneBySpotId(Integer spotId);
+
+    @Query("SELECT spot FROM PARKINGSPOT spot WHERE spot.parkingLot.parkingLotId = ?1 AND spot.spotId = ?2")
+    ParkingSpot findParkingSpotInLot(Integer parkingLotId, Integer spotId);
+
+    @Query("SELECT spot FROM PARKINGSPOT spot WHERE spot.parkingLot.parkingLotId = ?1 AND spot.vehicleType = ?2")
+    List<ParkingSpot> findParkingSpotsByType(Integer parkingLotId, VehicleType type);
+
+    @Query("SELECT spot FROM PARKINGSPOT spot WHERE spot.parkingLot.parkingLotId = ?1 AND spot.spotRent.vehiclePlate = ?2")
+    ParkingSpot findParkingSpotByVehiclePLate(Integer parkingLotId, String plate);
 }
