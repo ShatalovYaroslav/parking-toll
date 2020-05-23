ea12a66a0d101d2a783c4318aaacd5b516ec2951
Yaroslav_Shatalov <yaroslav.shatalov@activeeon.com>
2020-05-23 14:54:01 +0200

    fix configuration for DB

--- src/main/java/org/myproject/parking/Application.java
+++ src/main/java/org/myproject/parking/Application.java
@@ -1,13 +1,18 @@
 package org.myproject.parking;
 
 import com.google.common.base.Predicate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.autoconfigure.web.MultipartAutoConfiguration;
+import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.MediaType;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
@@ -19,12 +24,29 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import javax.sql.DataSource;
+import java.io.File;
+
 
 @SpringBootApplication
 @EnableAutoConfiguration(exclude = {MultipartAutoConfiguration.class})
 @EnableSwagger2
 @PropertySource("classpath:application.properties")
+@EntityScan(basePackages = "org.myproject.parking")
+@EnableTransactionManagement
 public class Application extends WebMvcConfigurerAdapter {
+
+    @Value("${spring.datasource.driverClassName:}")
+    private String dataSourceDriverClassName;
+
+    @Value("${spring.datasource.url:}")
+    private String dataSourceUrl;
+
+    @Value("${spring.datasource.username:}")
+    private String dataSourceUsername;
+
+    @Value("${spring.datasource.password:}")
+    private String dataSourcePassword;
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
@@ -52,7 +74,7 @@ public class Application extends WebMvcConfigurerAdapter {
     @Bean
     public Docket microserviceApi() {
         return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo())
-                .groupName("game")
+                .groupName("parking")
                 .select()
                 .paths(allowedPaths())
                 .build();
@@ -67,5 +89,32 @@ public class Application extends WebMvcConfigurerAdapter {
 
     private Predicate<String> allowedPaths() {
         return PathSelectors.regex("/parking.*");
+    }
+
+    @Bean
+    @Profile("default")
+    public DataSource defaultDataSource() {
+        String jdbcUrl = dataSourceUrl;
+
+        if (jdbcUrl.isEmpty()) {
+            jdbcUrl = "jdbc:hsqldb:file:" + getDatabaseDirectory() +
+                    ";create=true;hsqldb.tx=mvcc;hsqldb.applog=1;hsqldb.sqllog=0;hsqldb.write_delay=false";
+        }
+
+        return DataSourceBuilder.create()
+                .username(dataSourceUsername)
+                .password(dataSourcePassword)
+                .url(jdbcUrl)
+                .driverClassName(dataSourceDriverClassName)
+                .build();
+    }
+
+    private String getDatabaseDirectory() {
+        String projectHome = System.getProperty("parking.home");
+        if (projectHome == null) {
+            return System.getProperty("java.io.tmpdir") + File.separator + "parking";
+        }
+        return projectHome + File.separator + "data" + File.separator + "park" +
+                File.separator + "sv";
     }
 }
